be7cebc5af556072164f4f63d285bfc6f5a1ea25
kaustav <kaustavmanna4@gmail.com>
2024-02-18 01:36:06 +0530

    bug fix

--- src/main/java/com/quiz/quizapp/dao/QuizDao.java
+++ src/main/java/com/quiz/quizapp/dao/QuizDao.java
@@ -2,9 +2,6 @@ package com.quiz.quizapp.dao;
 
 import com.quiz.quizapp.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.UUID;
 
 public interface QuizDao extends JpaRepository<Quiz, UUID> {
